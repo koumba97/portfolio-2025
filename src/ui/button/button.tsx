5c3b5d6c5b9109d345f1d124af85cc1d7f42a751
Koumba KEITA <koumba.keita@epitech.eu>
2024-12-18 08:02:30 -0500

    Improve Button ui component

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -2,8 +2,9 @@ import "./button.scss";
 
 interface Prop {
     children: any;
+    type?: "filled" | "outlined";
 }
 
-export default function ButtonUI({ children }: Prop) {
-    return <div className="button dark">{children}</div>;
+export default function ButtonUI({ children, type = "filled" }: Prop) {
+    return <button className={`button ${type}`}>{children}</button>;
 }
