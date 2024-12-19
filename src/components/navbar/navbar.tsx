32f248f8f35b8c6f7eacaa9f12ea4be46a830735
Koumba KEITA <koumba.keita@epitech.eu>
2024-12-19 17:10:14 -0500

    Install FontAwesome

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,11 +2,15 @@ import LogoLink from "@/ui/logo-link/logo-link";
 import "./navbar.scss";
 import Link from "next/link";
 import ButtonUI from "../../ui/button/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAndroid } from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
     return (
         <div className="navbar">
             <LogoLink />
+            <FontAwesomeIcon icon={faBars} height={20} />
 
             <div className="links-container">
                 <Link href="">Home</Link>
