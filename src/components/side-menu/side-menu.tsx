79e43cff5cc02eded48c7f219b129b59651a886f
Koumba KEITA <koumba.keita@epitech.eu>
2024-12-19 17:50:19 -0500

    WIP SideMenu component

--- src/components/side-menu/side-menu.tsx
+++ src/components/side-menu/side-menu.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAndroid } from "@fortawesome/free-brands-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./side-menu.scss";
+import { useState } from "react";
 
 export default function SideMenu() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    function toggleSideMenu() {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div className="side-menu">
-            <FontAwesomeIcon icon={faBars} height={30} />
+            <FontAwesomeIcon
+                icon={faBars}
+                height={40}
+                className="menu-icon"
+                onClick={toggleSideMenu}
+            />
+            {isOpen ? "open" : "closed"}
         </div>
     );
 }
