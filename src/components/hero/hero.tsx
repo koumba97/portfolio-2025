2b14b59aaac570de222b7bfd955fd723c5a21980
Koumba KEITA <koumba.keita@epitech.eu>
2024-12-22 15:25:39 -0500

    Set up animated Hero title

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,9 +1,55 @@
+"use client";
+
 import koumImg from "@/assets/images/koum.png";
 import Image from "next/image";
 import "./hero.scss";
 import ButtonUI from "@/ui/button/button";
+import Movinwords from "movinwords";
+import "movinwords/styles";
+import { useEffect, useRef, useState } from "react";
 
 export default function Hero() {
+    const titleRef = useRef<Movinwords | null>(null);
+    const roles = [
+        "UI/UX Designer",
+        "Digital Illustrator",
+        "Frontend engineer",
+    ];
+    const [currentRole, setCurrentRole] = useState(roles[2]);
+
+    const INTERVAL_TIME = 3000;
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentRole((prev) => {
+                const nextIndex = (roles.indexOf(prev) + 1) % roles.length;
+                return roles[nextIndex];
+            });
+        }, INTERVAL_TIME);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    useEffect(() => {
+        if (titleRef.current) {
+            titleRef.current.destroy?.();
+            titleRef.current = null;
+        }
+
+        titleRef.current = new Movinwords({
+            el: ".test",
+            sentence: currentRole,
+            transition: "slideInBottom",
+        });
+
+        titleRef.current.start();
+
+        return () => {
+            titleRef.current?.destroy?.();
+            titleRef.current = null;
+        };
+    }, [currentRole]);
+
     return (
         <div className="hero-section section">
             <Image src={koumImg} alt="Koum image" id="koum-img" />
@@ -11,14 +57,11 @@ export default function Hero() {
                 <div className="text-container">
                     <div>
                         <h2>Hi I'm Koum and I'm a</h2>
-                        <h1 className="title">
-                            Frontend <br />
-                            <span className="brand">Engineer</span>
-                        </h1>
+                        <h1 className="title test"></h1>
                     </div>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        In in urna sit amet sapien accumsan consequat.{" "}
+                        In in urna sit amet sapien accumsan consequat.
                     </p>
                     <div className="buttons-container">
                         <ButtonUI>Learn more</ButtonUI>
