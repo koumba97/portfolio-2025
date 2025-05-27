5d4a1d181d17568dee721a80554088e54bf4fb44
Koumba KEITA <koumba.keita@epitech.eu>
2025-05-27 14:59:14 -0400

    Use MaterialUI components

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -12,6 +12,8 @@ import HomeSVG from "@/svg/Home";
 import SuitcaseSVG from "@/svg/Suitcase";
 import ToolSVG from "@/svg/Tool";
 import UserSVG from "@/svg/User";
+import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
 
 export default function NavBar() {
     const [scrollPosition, setScrollPosition] = useState(0);
@@ -71,21 +73,33 @@ function BottomNavbar({ scrollPosition }: NavbarProp) {
                 scrollPosition > 90 ? "slideup" : null
             }`}
         >
-            <div className="nav-button">
-                <HomeSVG width={24} height={24} viewBox="0 0 32 32" />
-            </div>
-            <div className="nav-button">
-                <UserSVG width={24} height={24} viewBox="0 0 24 24" />
-            </div>
-            <div className="nav-button">
-                <SuitcaseSVG width={24} height={24} viewBox="2 2 30 30" />
-            </div>
-            <div className="nav-button">
-                <FolderSVG width={22} height={22} viewBox="0 0 20 18" />
-            </div>
-            <div className="nav-button">
-                <ToolSVG width={24} height={24} viewBox="0 0 37 37" />
-            </div>
+            <Tooltip title="Home" placement="top">
+                <Button className="nav-button" color="inherit">
+                    <HomeSVG width={24} height={24} viewBox="0 0 32 32" />
+                </Button>
+            </Tooltip>
+            <Tooltip title="About me" placement="top">
+                <Button className="nav-button" color="inherit">
+                    <UserSVG width={24} height={24} viewBox="0 0 24 24" />
+                </Button>
+            </Tooltip>
+
+            <Tooltip title="Experience" placement="top">
+                <Button className="nav-button" color="inherit">
+                    <SuitcaseSVG width={24} height={24} viewBox="2 2 30 30" />
+                </Button>
+            </Tooltip>
+
+            <Tooltip title="Projects" placement="top">
+                <Button className="nav-button" color="inherit">
+                    <FolderSVG width={22} height={22} viewBox="0 0 20 18" />
+                </Button>
+            </Tooltip>
+            <Tooltip title="Skills" placement="top">
+                <Button className="nav-button" color="inherit">
+                    <ToolSVG width={24} height={24} viewBox="0 0 37 37" />
+                </Button>
+            </Tooltip>
         </div>
     );
 }
