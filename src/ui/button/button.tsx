3b130039a8a01e2677c76f077b672901ed1431f7
Koumba KEITA <koumba.keita@epitech.eu>
2025-05-27 16:44:36 -0400

    Use MaterialUI button component

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -1,10 +1,15 @@
 import "./button.scss";
+import Button from "@mui/material/Button";
 
 interface Prop {
     children: any;
-    type?: "filled" | "outlined";
+    type?: "contained" | "outlined";
 }
 
-export default function ButtonUI({ children, type = "filled" }: Prop) {
-    return <button className={`button ${type}`}>{children}</button>;
+export default function ButtonUI({ children, type = "contained" }: Prop) {
+    return (
+        <Button className={`button ${type}`} variant={type}>
+            {children}
+        </Button>
+    );
 }
