c5d013dbdf65b2824b7d6f7e2e2cd5e1377875fd
Koumba KEITA <koumba.keita@epitech.eu>
2025-06-22 22:31:29 -0400

    WIP Project gallery -> Set up fullscreen behavior

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "./style.scss";
-import { use } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
@@ -34,13 +34,44 @@ export default function ProjectPage({
     params: Promise<{ slug: string }>;
 }) {
     const { slug } = use(params);
+    const galleryRef = useRef<ImageGallery | null>(null);
+    const [isFullscreen, setIsFullscreen] = useState(false);
+
+    useEffect(() => {
+        const handleFullscreenChange = () => {
+            const isFullscreen = !!document.fullscreenElement;
+            setIsFullscreen(isFullscreen);
+            if (isFullscreen) {
+                document
+                    .getElementsByClassName("image-gallery")[0]
+                    .classList.add("fullscreen");
+            } else {
+                document
+                    .getElementsByClassName("image-gallery")[0]
+                    .classList.remove("fullscreen");
+            }
+        };
+
+        document.addEventListener("fullscreenchange", handleFullscreenChange);
+
+        return () => {
+            document.removeEventListener(
+                "fullscreenchange",
+                handleFullscreenChange
+            );
+        };
+    }, []);
 
     return (
         <div className="project-section section">
             <div className="grid">
                 <div className="grid-section">
                     <div className="back"></div>
-                    <ImageGallery items={images} />
+                    <ImageGallery
+                        ref={galleryRef}
+                        items={images}
+                        autoPlay={false}
+                    />
                 </div>
                 <div className="grid-section"></div>
                 <div className="grid-section"></div>
