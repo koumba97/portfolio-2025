c6220265f4504ca6a9dfe11ce0af9c32b8e8f694
Koumba KEITA <koumba.keita@epitech.eu>
2025-06-22 20:44:43 -0400

    WIP Project gallery

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -2,6 +2,31 @@
 
 import "./style.scss";
 import { use } from "react";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+const images = [
+    {
+        original: "https://picsum.photos/id/1018/1000/600/",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+];
 
 export default function ProjectPage({
     params,
@@ -11,16 +36,17 @@ export default function ProjectPage({
     const { slug } = use(params);
 
     return (
-        <div className="project-section">
+        <div className="project-section section">
             <div className="grid">
-                <div>
+                <div className="grid-section">
                     <div className="back"></div>
+                    <ImageGallery items={images} />
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="grid-section"></div>
+                <div className="grid-section"></div>
+                <div className="grid-section"></div>
+                <div className="grid-section"></div>
+                <div className="grid-section"></div>
             </div>
         </div>
     );
